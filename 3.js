0a426f631983d47ff6d03202d239f2f1eb9c5bad
Guilherme Santos <guilherme.tretow@gmail.com>
2022-06-27 16:13:51 -0300

    playing with tests

--- 3.js
+++ 3.js
@@ -10,11 +10,22 @@ result = subtract(7, 3)
 expected = 4
 expect(result).toBe(expected)
 
+let resultLength = "test"
+let expectedLength = "result"
+
+expect(resultLength).toBe(expectedLength)
+
+
 function expect(actual) {
   return {
     toBe(expected) {
       if (actual !== expected) {
         throw new Error(`${actual} is not equal to ${expected}`)
+      }
+    },
+    toHaveLength(expected) {
+      if (actual.length !== expected.length) {
+        throw new Error(`"${actual}" length is not equal to "${expected}" length`)
       }
     },
   }
